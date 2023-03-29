e41ddf58151c60ebc5a7e72c83b9484f38106cde
shiumano <takkunmail2005@gmail.com>
2023-03-29 15:02:12 +0900

    まちきれない！

--- bot.py
+++ bot.py
@@ -5,10 +5,20 @@ from config import TOKEN
 
 bot = commands.Bot(command_prefix='y%')
 
+started = False
+
 @bot.event
 async def on_ready():
     print('Bot ready')
     for file in glob.glob("cogs/*.py"):
         await bot.load_extension(file.replace('/', '.')[:-3])
+
+    global started
+    started = True
+
+@bot.event
+async def on_ready(message):
+    if not started:
+        await on_ready()
 
 bot.run(TOKEN)
